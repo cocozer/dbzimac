dc35325f21a6d046b9416c519e41143c91e2f163
cocozer <corentin.pak@gmail.com>
2022-12-12 22:49:41 +0100

    sdl

--- src/application_ui.h
+++ src/application_ui.h
@@ -1,6 +1,6 @@
 #ifndef APPLICATION_UI_H
 #define APPLICATION_UI_H
-#include <SDL2/SDL.h>
+#include <SDL2/x86_64-w64-mingw32/include/SDL2/SDL.h>
 #include <string>
 
 extern const int SCREEN_WIDTH, SCREEN_HEIGHT;
